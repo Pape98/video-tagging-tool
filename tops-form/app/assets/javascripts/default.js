62a099d43a33f630a0015b4226a0813a03bf6b8a
Pape98 <papisline98@hotmail.fr>
2019-05-05 04:19:04 -0500

    got rid of hidden field

--- tops-form/app/assets/javascripts/default.js
+++ tops-form/app/assets/javascripts/default.js
@@ -2,25 +2,40 @@ function fillData(){
 
     var courses =  $('#courses-label').data('courses');
     var keywords =  $('#keywords-label').data('keywords');
-    var i;
-    for (i = 1; i < courses.length; i++) {
-        var course = courses[i];
-        var selection = "<a class=\"ui label transition visible\"data-value=" +
-            course + " style=\"display: inline-block !important;\">" +
-            course+"<i class=\"delete icon\"></i></a>";
-        $('#courses + i').after(selection);
-        $("#courses").val(course);
-        console.log($("#courses").val());
-    }
+    var cuts =  $('#cuts-label').data('cuts');
 
-    for (i = 1; i < keywords.length; i++) {
-        var keyword = keywords[i];
-        var selection = "<a class=\"ui label transition visible\"data-value=" +
-            keyword + " style=\"display: inline-block !important;\">" +
-            keyword+"<i class=\"delete icon\"></i></a>";
-        $('#keywords + i').after(selection);
-        $("#keywords").val(keyword);
-        $("#keywords").val();
+    var i;
+    if(courses != undefined){
+        for (i = 0; i < courses.length; i++) {
+            var course = courses[i];
+            var selection = "<a class=\"ui label transition visible\"data-value=" +
+                course + " style=\"display: inline-block !important;\">" +
+                course+"<i class=\"delete icon\"></i></a>";
+            $('#courses + i').after(selection);
+            $("#courses").val(course);
+        }
+
+        for (i = 0; i < keywords.length; i++) {
+            var keyword = keywords[i];
+            console.log(keyword)
+            var selection = "<a class=\"ui label transition visible\"data-value=" +
+                keyword + " style=\"display: inline-block !important;\">" +
+                keyword+"<i class=\"delete icon\"></i></a>";
+            $('#keywords + i').after(selection);
+            $("#keywords").val(keyword);
+        }
+
+        for (i = 0; i < cuts.length; i++) {
+            var cut = cuts[i];
+            console.log(cut)
+            var option = "<option value=\""+cut+"\">"+cut+"</option>";
+            var selection = "<a class=\"ui label transition visible\"data-value=" +
+                cut + " style=\"display: inline-block !important;\">" +
+                cut+"<i class=\"delete icon\"></i></a>";
+            $('#cuts').append(option);
+            $('#cuts + i').after(selection);
+            $("#cuts").val(cut);
+        }
     }
 }
 
--- tops-form/app/helpers/videos_helper.rb
+++ tops-form/app/helpers/videos_helper.rb
@@ -2,14 +2,22 @@ module VideosHelper
 
   def courses
     [
-        "141 - Business Math I",
-        "142 - Business Math II",
-        "150 - PreCalculus",
-        "151 - Calculus I",
-        "152 - Calculus II",
-        "251 - Calculus III",
-        "304 - Linear Algebra",
-        "308 Differential Equations"
+        # "141 - Business Math I",
+        # "142 - Business Math II",
+        # "150 - PreCalculus",
+        # "151 - Calculus I",
+        # "152 - Calculus II",
+        # "251 - Calculus III",
+        # "304 - Linear Algebra",
+        # "308 Differential Equations"
+        "141",
+        "142",
+        "150",
+        "151",
+        "152",
+        "251",
+        "304",
+        "308"
     ]
   end
 

9be17612d2a1c4b512f516ee25202a0dddc8b6ae
Pape98 <papisline98@hotmail.fr>
2019-07-15 22:16:19 -0500

    fixed labels

--- tops-form/app/assets/javascripts/default.js
+++ tops-form/app/assets/javascripts/default.js
@@ -13,8 +13,9 @@ function fillData(){
     for(var i = 0; i < x.length; ++i){
         var keywords = $(x[i]).data('keywords');
         var cut = $(x[i]).data('cut');
-        console.log(cut)
-        $('.'+cut).dropdown('set selected',keywords);
+        if(cut != ''){
+            $('.'+cut).dropdown('set selected',keywords);
+        }
     }
     update_ui()
 }

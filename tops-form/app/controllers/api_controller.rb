78402836c6e1bd23101df61f7a2066b5a9d7b681
traorefly <traorefly@tamu.edu>
2020-04-03 08:50:42 -0500

    add keywords

--- tops-form/app/controllers/api_controller.rb
+++ tops-form/app/controllers/api_controller.rb
@@ -1,13 +1,13 @@
 class ApiController < ApplicationController
     # Get /videos
     def videos
-        videos = Video.only('courses','link','topic,keywords','presenter')
+        videos = Video.only('courses','link','topic','keywords','presenter','resources')
         render json: videos
     end 
 
     def video
         id = params[:id]
-        video = Video.only('courses','link','topic,keywords','presenter').find(id)
+        video = Video.only('courses','link','topic','keywords','presenter','resources').find(id)
         render json: video
     end 
 end
